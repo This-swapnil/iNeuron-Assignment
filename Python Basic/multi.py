79a44c1337e2ee6c2e07b74a0ccceb3e58c93eb2
swapnil sonawane <swapnil@pop-os.localdomain>
2021-11-18 11:05:26 +0530

    first commit

---
+++ Python Basic/multi.py
@@ -0,0 +1,12 @@
+import multiprocessing
+from datetime import datetime
+from time import sleep
+import random
+def printsec(seconds):
+    sleep(seconds)
+    print('wait',seconds,'seconds, time is',datetime.utcnow())
+if __name__ =="__main__":
+    for i in range(3):
+        seconds = random.random()
+        proc = multiprocessing.Process(target=printsec,args=(seconds,))
+        proc.start()
